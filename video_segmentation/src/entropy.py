42c2527cb6bc65a26b6bfd47ab1e19da68e2c49d
AayushGrover <Aayush.Grover@iiitb.org>
2020-04-30 13:16:54 +0530

    Fixed bug-printing local minimas

--- video_segmentation/src/entropy.py
+++ video_segmentation/src/entropy.py
@@ -3,9 +3,10 @@ import math
 import matplotlib.pyplot as plt; plt.rcdefaults() 
 import datetime
 
-def get_entropy(all_frames, window_size=20):
+def get_entropy(all_frames, n, window_size=20):
     frames = []
-    for i in range(2,490):
+
+    for i in range(2,n-window_size-2):
         l = all_frames[i:i+window_size]
         entropy = np.var(l, axis=0)  # Calculate variance
         l1_norm = np.linalg.norm(entropy)
@@ -36,15 +37,16 @@ def smoothen(curve, k):
     
     return new_curve
 
-def plot(frames, smoothing_factor):
+def plot(frames, n, smoothing_factor, window_size):
     y_pos = list()
-    for i in range(488):
+    for i in range(n-window_size-4):
         y_pos.append(i)
     performance = frames 
     smooth_performance = smoothen(curve=performance, k=smoothing_factor)
 
     plt.xlabel("timestamp windows")
     plt.ylabel("Norm of entropy")
+    print(len(y_pos), len(performance))
     plt.bar(y_pos, performance, align='center', alpha=0.5)
     plt.plot(y_pos, smooth_performance, 'r')
     plt.show()
@@ -53,7 +55,7 @@ def plot(frames, smoothing_factor):
 
 def check(timestamp, seq, l):
     for t,_ in seq:
-        if abs((t/l*300)-(timestamp/l*300)) < 2:
+        if abs((t/l*300)-(timestamp/l*300)) < 5:
             return False
     return True
 
@@ -63,12 +65,17 @@ def get_top_k_performance(y_pos, performance_list, k):
         performance_for_top_k.append((x, y))
     performance_for_top_k.sort(key=lambda x: x[1])
     # top_k_pairs = performance_for_top_k[:k]
+    # print(performance_for_top_k[:10])
     top_k_pairs = [performance_for_top_k[0]]
     k_count = 1
     for t,performance in performance_for_top_k:
-        if abs(performance - top_k_pairs[-1][1]) > 0.2 and check(t,top_k_pairs,len(performance_list)):
+        if check(t,top_k_pairs,len(performance_list)):
             top_k_pairs.append((t,performance))
             k_count += 1
+        else:
+            if(abs(performance - top_k_pairs[-1][1]) > 3):
+                top_k_pairs.append((t,performance))
+                k_count += 1    
         if k_count == k:
             break
 
@@ -76,13 +83,15 @@ def get_top_k_performance(y_pos, performance_list, k):
     top_k_pairs.sort(key=lambda x: x[0])
     for (t, _) in top_k_pairs:
         print(f'Frame at which segmentation should happen: {t}, Time at which segmentation should happen: {datetime.timedelta(seconds=(t/len(performance_list)*300))}')
+        # print(f'Frame at which segmentation should happen: {t}, Time at which segmentation should happen: {(t/len(performance_list)*300)}')
 
 # So we can check if the enropy for the k+1 th frame increased or decreased and decide the split accordingly
 
 if __name__ == "__main__":
     
-    b = np.load('feature_Segment01.npy')
-    
-    frames = get_entropy(b)
-    y_pos, performance = plot(frames, smoothing_factor=3)
-    get_top_k_performance(y_pos, performance,k=15)
+    b = np.load('../data/Celein/feature_Segment01.npy')
+    window_size = 20
+
+    frames = get_entropy(b, n=b.shape[0], window_size=window_size)
+    y_pos, performance = plot(frames, n=b.shape[0], smoothing_factor=3, window_size=window_size)
+    get_top_k_performance(y_pos, performance,k=10)
